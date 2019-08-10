2fb4e35fd8f437378d604097d2375c9d82392e6a
xuyongming520 <1692679304@qq.com>
2019-08-10 14:44:53 +0800

    demo 完成

--- src/todolist/tabs.vue
+++ src/todolist/tabs.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="helper">
-    <span class="left">2 items left</span>
+    <span class="left">{{unFinishedTodoLength}} items left</span>
     <span class="tabs">
       <span
         v-for="state in states"
@@ -21,6 +21,10 @@ export default {
     filter:{
       type:String,
       required:true,
+    },
+    todos:{
+      type:Array,
+      required:true,
     }
   },
   data(){
@@ -28,9 +32,18 @@ export default {
       states:['all','active','completed']
     }
   },
+  computed:{
+    unFinishedTodoLength(){
+      return this.todos.filter(todo => !todo.completed).length
+    }
+  },
   methods:{
-    toggleFilter(state){},
-    clearAllCompleted(){}
+    toggleFilter(state){
+      this.$emit('toggle', state)
+    },
+    clearAllCompleted(){
+      this.$emit('clearAll')
+    }
   }
 }
 </script>
