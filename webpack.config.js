7942bfd7fe1a63d2b977689c48d16dc892d7a0f1
xuyongming520 <1692679304@qq.com>
2019-08-11 02:09:07 +0800

    css 分离打包

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,13 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const isDev = process.env.NODE_ENV === 'development'
 const HTMLPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const config = {
   target: 'web',
   entry: path.join(__dirname, "src/index.js"),
   output:{
-    filename:'bundle.js',
+    filename:'bundle.[hash:8].js',
     path:path.join(__dirname,'dist')
   },
   module:{
@@ -20,27 +21,6 @@ const config = {
       {
         test: /\.jsx$/,
         loader:"babel-loader"
-      },
-      {
-        test: /\.css$/,
-        use:[
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.styl/,
-        use:[
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          'stylus-loader'
-        ]
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -68,6 +48,20 @@ const config = {
 }
 
 if(isDev){
+  config.module.rules.push({
+    test: /\.styl/,
+    use:[
+      'style-loader',
+      'css-loader',
+      {
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true
+        }
+      },
+      'stylus-loader'
+    ]
+  })
   config.devtool = '#cheap-module-eval-source-map'
   config.devServer = {
     port: 8000,
@@ -82,6 +76,30 @@ if(isDev){
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
   )
+}else{
+  config.output.filename = '[name].[chunkhash:8].js'
+  config.module.rules.push(
+    {
+      test: /\.styl/,
+      use:ExtractPlugin.extract({
+        fallback:'style-loader',
+        use:[
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          'stylus-loader'
+        ]
+      })
+    }  
+  )
+  config.plugins.push(
+    new ExtractPlugin('styles.[chunkHash:8].css')
+  )
 }
 
 module.exports = config
+
